3bb95afd37ab8aed41db542435d282c6f88ea481
Ilja Baranovkis <ilja.baranovskis90@gmail.com>
2021-12-05 11:26:11 +0200

    Ilja Baranovskis Final Project commit

--- src/test/java/MyProject/testRunner/OnlineShopTests.java
+++ src/test/java/MyProject/testRunner/OnlineShopTests.java
@@ -70,29 +70,3 @@ public class OnlineShopTests extends TestBase {
         billingPage.setContinueButton();
     }
 }
-//        homePage.setSearchField(ITEM);
-//        Thread.sleep(DEFAULT_SLEEP_VALUE);
-//        productPage.setSelectComputer(productConstants.getProductName());
-//        assertEquals("Stacionārs dators Komputronik Ultimate X711 [D1], Nvidia GeForce RTX 3070", computerPage.getProductTitle());
-//        assertEquals("2.141,74", computerPage.getProductPrice(productConstants.getItemPrice()));
-//        computerPage.selectAddToCart();
-//        Thread.sleep(DEFAULT_SLEEP_VALUE);
-//        computerPage.setBrowseShoppingCart();
-//        shoppingCartPage.setPaymentMethod();
-//        authenticationPage.setEmailField(customerConstants.getEmail());
-//        authenticationPage.clickContinueButton();
-//        userDataPage.setFirstNameField(customerConstants.getFirstName());
-//        userDataPage.setLastNameField(customerConstants.getLastName());
-//        userDataPage.setPhoneNumberField(customerConstants.getPhoneNumber());
-//        userDataPage.setCityField(customerConstants.getCity());
-//        userDataPage.setAddressField(customerConstants.getAddress());
-//        userDataPage.setPostcodeField(customerConstants.getPostCode());
-//        userDataPage.setContinueButton();
-//        checkoutPage.clickRadioButtonClientsCenter();
-//        Thread.sleep(DEFAULT_SLEEP_VALUE);
-//        checkoutPage.clickRadioButtonShopCenter();
-//        Thread.sleep(DEFAULT_SLEEP_VALUE);
-//        checkoutPage.continueButton();
-//        billingPage.setCheckBoxPrivacyPolicy();
-//        billingPage.setContinueButton();
-//    }
